1e246f70fc72448ac2c9565ef78341d2766ff7bd
arthurtruong <arthurtruong.work@gmail.com>
2018-10-31 00:20:12 +1100

    jest test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Config
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
 });
+
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
+
+/**
+* Factory function to create a ShallowWrapper for the App Component
+* @Function setup
+* @params {object} props - Component props specific to this setup
+* @param {any} state - Initial state for setup
+* @returns { ShallowWrapper}
+const setup = (props={}, state=null) => {
+  return shallow (<App {...props />})
+}
+*/
+
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param { ShallowWrapper } wrapper - Enzyme shallow wrapper to search within
+ * @returns {ShallowWrapper}
+ */
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+test("renders without errors", () => {
+  const wrapper = setup();
+  // <App /> = component-app
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders increment button", () => {
+  const wrapper = setup();
+  // <App /> = component-app
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = setup();
+  // <App /> = component-app
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
+
+// testing of the state
+test("counter starts at 0", () => {});
+
+test("clicking button increments counter display", () => {});
